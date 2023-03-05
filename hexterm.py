e8ecf71b3d61aad311bf08503e324fdad8988ddc
Theodore Kotz <ted@kotz.us>
2023-03-05 10:00:56 -0500

    Some older changes I forgot to commit

--- hexterm.py
+++ hexterm.py
@@ -1,31 +1,58 @@
 #!/usr/bin/env python3
 
+import threading
+import io
+import sys
+import time
+
 running = True;
 
-def Char2BytesLoop():
+def ConvertBytes2String(mybytes):
+    return mybytes.decode()
+
+def ConvertString2Bytes(mystring):
+    return mystring.encode()
+
+def Char2BytesLoop(charInput, bytesOutput):
     global running
     while running:
+        time.sleep(1)
         line = charInput.readline()
-        if line[0].upper == "Q":
-            runnning=False
+        if line[0].upper() == "Q":
+            running=False
         else:
             bytesOutput.write(ConvertString2Bytes(line))
 
-def Bytes2CharLoop():
+def Bytes2CharLoop(bytesInput, charOutput):
     global running
     count=0
     while running:
+        time.sleep(1)
         if count < 16:
-            charOutput.write(ConvertBytes2String(bytesInput.read()), end=" ")
+            charOutput.write(ConvertBytes2String(bytesInput.read(1))+" ")
             ++count
         else:
-            charOutput.write(ConvertBytes2String(bytesInput.read()))
+            charOutput.write(ConvertBytes2String(bytesInput.read(1))+"\n")
             count=0
 
 
 
-def mainloop(charInput, charOutput, bytesInput, bytesOutout):
-     // Start both loops.
-     // wait for join.
+def mainloop():
+    bbuf = io.BytesIO()
+    b2c = threading.Thread(target=Bytes2CharLoop, args=(bbuf,sys.stdout))
+    c2b = threading.Thread(target=Char2BytesLoop, args=(sys.stdin,bbuf))
 
+    # Start both loops.
+    b2c.start()
+    c2b.start()
+
+    # wait for join.
+    b2c.join()
+    c2b.join()
+
+    print("Exiting")
+    print(bbuf)
+
+
+mainloop()
 
